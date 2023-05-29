0f9669fc1fabda7426cfa2023d8b542931c54f23
vinicius-moreira-maia <viniciusconcept@gmail.com>
2023-05-28 22:56:31 -0300

    Semana 2 finalizada

---
+++ USP-Python2/s2/lista/maiusculas.py
@@ -0,0 +1,15 @@
+def main():
+    print(maiusculas('Programamos em python 2?'))
+    print(maiusculas('PrOgRaMaMoS em python!'))
+
+def maiusculas(frase):
+    res = ''
+
+    for letra in frase:
+        if letra.isupper():
+            res += letra
+    
+    return res
+
+if __name__ == '__main__':
+    main()
