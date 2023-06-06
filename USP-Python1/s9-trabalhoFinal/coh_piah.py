feb96c897ad0c4ba1755fa3ca38283b49b13904b
vinicius-moreira-maia <viniciusconcept@gmail.com>
2023-06-06 14:29:50 -0300

    Semana 9 do curso 1 da USP finalizada

--- USP-Python1/s9-trabalhoFinal/coh_piah.py
+++ USP-Python1/s9-trabalhoFinal/coh_piah.py
@@ -1,5 +1,13 @@
 import re
 
+def main():
+    ass = le_assinatura()
+    textos = le_textos()
+    texto_infectado = avalia_textos(textos, ass)
+
+    print(f"O autor do texto {texto_infectado} está infectado com COH-PIAH")
+
+
 def le_assinatura():
     '''A funcao le os valores dos tracos linguisticos do modelo e devolve uma assinatura a ser comparada com os textos fornecidos'''
     print("Bem-vindo ao detector automático de COH-PIAH.")
@@ -14,6 +22,7 @@ def le_assinatura():
 
     return [wal, ttr, hlr, sal, sac, pal]
 
+
 def le_textos():
     '''A funcao le todos os textos a serem comparados e devolve uma lista contendo cada texto como um elemento'''
     i = 1
@@ -26,6 +35,7 @@ def le_textos():
 
     return textos
 
+
 def separa_sentencas(texto):
     '''A funcao recebe um texto e devolve uma lista das sentencas dentro do texto'''
     sentencas = re.split(r'[.!?]+', texto)
@@ -33,13 +43,16 @@ def separa_sentencas(texto):
         del sentencas[-1]
     return sentencas
 
+
 def separa_frases(sentenca):
     '''A funcao recebe uma sentenca e devolve uma lista das frases dentro da sentenca'''
     return re.split(r'[,:;]+', sentenca)
 
+
 def separa_palavras(frase):
     '''A funcao recebe uma frase e devolve uma lista das palavras dentro da frase'''
     return frase.split()
+
 
 def n_palavras_unicas(lista_palavras):
     '''Essa funcao recebe uma lista de palavras e devolve o numero de palavras que aparecem uma unica vez'''
@@ -57,6 +70,7 @@ def n_palavras_unicas(lista_palavras):
 
     return unicas
 
+
 def n_palavras_diferentes(lista_palavras):
     '''Essa funcao recebe uma lista de palavras e devolve o numero de palavras diferentes utilizadas'''
     freq = dict()
@@ -69,14 +83,121 @@ def n_palavras_diferentes(lista_palavras):
 
     return len(freq)
 
+
 def compara_assinatura(as_a, as_b):
     '''IMPLEMENTAR. Essa funcao recebe duas assinaturas de texto e deve devolver o grau de similaridade nas assinaturas.'''
-    pass
+    ass = []
 
-def calcula_assinatura(texto):
-    '''IMPLEMENTAR. Essa funcao recebe um texto e deve devolver a assinatura do texto.'''
-    pass
+    for i in range(len(as_a)):
+        ass.append(abs(as_a[i] - as_b[i]))
+    
+    soma = sum(ass)
+
+    return soma / 6
+
 
 def avalia_textos(textos, ass_cp):
     '''IMPLEMENTAR. Essa funcao recebe uma lista de textos e uma assinatura ass_cp e deve devolver o numero (1 a n) do texto com maior probabilidade de ter sido infectado por COH-PIAH.'''
-    pass
+
+    suspeitos = []
+
+    for texto in textos:
+        ass_texto = calcula_assinatura(texto)
+        res_comparacao = compara_assinatura(ass_texto, ass_cp)
+        suspeitos.append(res_comparacao)
+    
+    indice_infectado = suspeitos.index(min(suspeitos))
+
+    return indice_infectado + 1
+    
+
+def calcula_assinatura(texto):
+    '''IMPLEMENTAR. Essa funcao recebe um texto e deve devolver a assinatura do texto.'''
+    assinatura = []
+
+    sentencas = separa_sentencas(texto)
+    frases = get_frases(sentencas)
+    palavras = get_palavras(frases)
+
+    tmp = tam_medio_palavras(palavras)
+    assinatura.append(tmp)
+    rtt = rel_type_token(palavras)
+    assinatura.append(rtt)
+    rhl = razao_hapax_legomana(palavras)
+    assinatura.append(rhl)
+    tms = tam_medio_sentenca(sentencas)
+    assinatura.append(tms)
+    cs = complexidade_sentenca(frases, sentencas)
+    assinatura.append(cs)
+    tmf = tam_medio_frase(frases)
+    assinatura.append(tmf)
+
+    return assinatura
+    
+# início das funções implementadas por mim
+
+def get_frases(sentencas):
+    frases = []
+
+    for sentenca in sentencas:
+        frases.extend(separa_frases(sentenca))
+    
+    return frases
+
+
+def get_palavras(frases):
+    palavras = []
+
+    for frase in frases:
+        palavras.extend(separa_palavras(frase))
+    
+    return palavras
+
+
+def tam_medio_palavras(palavras):
+    n_palavras = len(palavras)
+    
+    soma = 0
+
+    for palavra in palavras:
+        soma += len(palavra)
+    
+    return soma / n_palavras
+
+
+def rel_type_token(palavras):
+    return n_palavras_diferentes(palavras) / len(palavras)
+
+
+def razao_hapax_legomana(palavras):
+    return n_palavras_unicas(palavras) / len(palavras)
+
+
+def tam_medio_sentenca(sentencas):
+    soma = 0
+
+    for sentenca in sentencas:
+        for _ in sentenca:
+            soma += 1
+    
+    return soma / len(sentencas)
+
+
+def complexidade_sentenca(frases, sentencas):
+    return len(frases) / len(sentencas)
+
+
+def tam_medio_frase(frases):
+    soma = 0
+
+    for frase in frases:
+        for _ in frase:
+            soma += 1
+    
+    return soma / len(frases)
+
+# fim das funções implementadas por mim
+    
+
+if __name__ == '__main__':
+    main()
